22bb680ef58c92eedc89984f1026515b16540890
Nikkala Thomson <tho15058@byui.edu>
2019-05-04 19:14:32 -0400

    added landing page and links page

--- web/index.php
+++ web/index.php
@@ -1,5 +1,37 @@
-<?php
+<!DOCTYPE html>
+<!--
+This is the home page for Board Games for Families
+Author: Nikkala Thomson
+-->
 
-  phpinfo();
+<html lang="en-us">
 
-?>
+<head>
+    <?php require_once("modules/config.php");
+        include (ROOT_PATH . "modules/head.php"); ?>
+    <title>Home | Board Games for Families</title>
+</head>
+
+<body>
+    <header>
+        <?php require_once("modules/config.php");
+        include (ROOT_PATH . "modules/header.php"); ?>
+    </header>
+    <div class=center-block>
+        <nav>
+            <?php include ("modules/nav.php"); ?>
+        </nav>
+        <main>
+            <figure class=center-block>
+                <!--Main image -->
+                <img src="images/familygame.jpg" alt="Family playing games">
+            </figure>
+
+            <footer>
+                <?php include ("modules/footer.php"); ?>
+            </footer>
+        </main>
+    </div>
+</body>
+
+</html>
