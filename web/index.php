62b2f252a700f288c41ce3fd8351d4692a67d0c6
Nikkala Thomson <tho15058@byui.edu>
2019-05-04 19:53:39 -0400

    fixed error in index nav

--- web/index.php
+++ web/index.php
@@ -17,8 +17,10 @@ Author: Nikkala Thomson
     </header>
     <div class=center-block>
         <nav>
-            <li id="active-nav"><a href=".">Home<img src="images/yellow-arrow.png" alt=""></a></li>
-            <li><a href="links.php">Links</a></li>
+            <ul>
+                <li id="active-nav"><a href=".">Home<img src="images/yellow-arrow.png" alt=""></a></li>
+                <li><a href="links.php">Links</a></li>
+            </ul>
         </nav>
         <main>
             <figure class=center-block>
