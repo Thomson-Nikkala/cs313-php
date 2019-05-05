6e06b1aee87ee9fb6c33b1dcbe780a337f2d5da9
Nikkala Thomson <tho15058@byui.edu>
2019-05-04 20:02:40 -0400

    added games list to index

--- web/index.php
+++ web/index.php
@@ -27,6 +27,19 @@ Author: Nikkala Thomson
                 <!--Main image -->
                 <img src="images/familygame.jpg" alt="Family playing games">
             </figure>
+            <div class="flex">
+                <section>
+                    <p>Playing board games with your families creates fond memories and lasting experiences. Consider trying out one of these board games today!</p>
+
+                </section>
+                <section>
+                    <li><a href="https://boardgamegeek.com/boardgame/148228/splendor" target="_blank">07 Splendor</a></li>
+                    <li><a href="https://boardgamegeek.com/boardgame/9209/ticket-ride" target="_blank">08 Ticket to Ride</a></li>
+                    <li><a href="https://boardgamegeek.com/boardgame/199561/sagrada" target="_blank">09 Sagrada</a></li>
+                    <li><a href="https://boardgamegeek.com/boardgame/230802/azul" target="_blank">10 Azul</a></li>
+                </section>
+            </div>
+
         </main>
         <footer>
             <?php include("modules/footer.php"); ?>
