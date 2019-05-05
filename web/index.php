7ed616bd9b144e2994af16bd5642794875f28ef6
Nikkala Thomson <tho15058@byui.edu>
2019-05-04 20:07:15 -0400

    added one more game

--- web/index.php
+++ web/index.php
@@ -29,14 +29,17 @@ Author: Nikkala Thomson
             </figure>
             <div class="flex">
                 <section>
-                    <p>Playing board games with your families creates fond memories and lasting experiences. Consider trying out one of these board games today!</p>
+                    <p>Playing board games with your families creates lasting memories and precious experiences. Consider trying out one of these board games today!</p>
 
                 </section>
                 <section>
-                    <li><a href="https://boardgamegeek.com/boardgame/148228/splendor" target="_blank">07 Splendor</a></li>
-                    <li><a href="https://boardgamegeek.com/boardgame/9209/ticket-ride" target="_blank">08 Ticket to Ride</a></li>
-                    <li><a href="https://boardgamegeek.com/boardgame/199561/sagrada" target="_blank">09 Sagrada</a></li>
-                    <li><a href="https://boardgamegeek.com/boardgame/230802/azul" target="_blank">10 Azul</a></li>
+                    <ul>
+                        <li><a href="https://boardgamegeek.com/boardgame/148228/splendor" target="_blank">Splendor</a></li>
+                        <li><a href="https://boardgamegeek.com/boardgame/9209/ticket-ride" target="_blank">Ticket to Ride</a></li>
+                        <li><a href="https://boardgamegeek.com/boardgame/199561/sagrada" target="_blank">Sagrada</a></li>
+                        <li><a href="https://boardgamegeek.com/boardgame/230802/azul" target="_blank">Azul</a></li>
+                        <li><a href="https://boardgamegeek.com/boardgame/266192/wingspan" target="_blank">Wingspan</a></li>
+                    </ul>
                 </section>
             </div>
 
