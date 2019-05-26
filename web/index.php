d1ea7fdea6bb393a90ac094c8fc88b5669f8e92a
Nikkala Thomson <tho15058@byui.edu>
2019-05-25 21:05:48 -0400

    updating index with link

--- web/index.php
+++ web/index.php
@@ -22,6 +22,7 @@ Author: Nikkala Thomson
                 <li id="active-nav"><a href=".">Home<img src="images/yellow-arrow.png" alt=""></a></li>
                 <li><a href="links.php">Links</a></li>
                 <li><a href="shopping/index.php">Shopping</a></li>
+                <li><a href="boardgames/index.php">Games</a></li>
             </ul>
         </nav>
         <main>
