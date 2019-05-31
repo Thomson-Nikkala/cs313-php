9cd27d042d6b69f4f666c5887c1a90bc96c1118d
Nikkala Thomson <tho15058@byui.edu>
2019-05-31 19:24:20 -0400

    testing

--- web/boardgames/games.php
+++ web/boardgames/games.php
@@ -68,15 +68,11 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);  -->
                  $player_preferences = $player['preferences'];   // this is a string
                  $player_prefs_json = json_decode($player_preferences);  // coerce to json object
                  $min_players_pref = $player_prefs_json->min_players;
-                 echo $min_players_pref;
-                echo gettype(@min_players_pref);
                 ?>
                 <form action="games.php" method="post">
                     <?php 
                       $min_players = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
-                      $max_players = array('one'=>'1', 'two'=>'2', 'three'=>'3', 'four'=>'4', 'five'=>'5',
-                                        'six'=>'6', 'seven'=>'7', 'eight'=>'9', 'nine'=>'9', 'ten'=>'10+');
-                   //($player_prefs_json->min_players)
+                      $max_players = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
                     ?>
                     <p>Minimum number of players:</p> <select name="min_players">
                         <?php foreach( $min_players as $min_player ): ?>
