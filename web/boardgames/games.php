2424217bb83564792c3affb45241296b9e75f328
Nikkala Thomson <tho15058@byui.edu>
2019-05-31 17:59:59 -0400

    testing

--- web/boardgames/games.php
+++ web/boardgames/games.php
@@ -71,9 +71,9 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);  -->
                 <form action="games.php" method="post">
                     <?php 
                       $min_players = array('one'=>1, 'two'=>2, 'three'=>3, 'four'=>4, 'five'=>5,
-                                        'six'=>6, 'seven'=>, 'eight'=>, 'nine'=>9, 'ten'=>10);
+                                        'six'=>6, 'seven'=>7, 'eight'=>8, 'nine'=>9, 'ten'=>10);
                       $max_players = array('one'=>1, 'two'=>2, 'three'=>3, 'four'=>4, 'five'=>5,
-                                        'six'=>6, 'seven'=>, 'eight'=>, 'nine'=>9, 'ten'=>10+);
+                                        'six'=>6, 'seven'=>7, 'eight'=8>, 'nine'=>9, 'ten'=>10+);
                     ?>
                     <p>Minimum number of players:</p> <select name="min_players">
                         <?php foreach( $min_players as $var => $min_player ): ?>
