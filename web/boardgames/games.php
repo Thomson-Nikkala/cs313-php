ec87bcbf74fc046de6b07be6fd4da518d40db609
Nikkala Thomson <tho15058@byui.edu>
2019-05-31 11:53:27 -0400

    testing

--- web/boardgames/games.php
+++ web/boardgames/games.php
@@ -60,10 +60,11 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);  -->
                 <?php
                 echo 'testing';
                  $query = "SELECT preferences->>'min_players' AS 'min_players' FROM preference p WHERE p.gamer = " . $_SESSION["gamer"];
+                echo 'testing2';
                   $statement = $db->prepare($query);
                     $statement->execute();   
                     $min_player_data = $statement->fetch(PDO::FETCH_ASSOC);
-                echo 'testing2';
+               // echo $min_player_data['min_players'] . 'testing2';
                 ?>
 
 
