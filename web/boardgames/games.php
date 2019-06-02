4d188fe8717d3a5fe4484baa3cf4dbaa8ab07cff
Nikkala Thomson <tho15058@byui.edu>
2019-06-01 20:21:54 -0400

    testing

--- web/boardgames/games.php
+++ web/boardgames/games.php
@@ -139,7 +139,7 @@ Author: Nikkala Thomson
                     <br>
 
                     <p>Get a game recommendation based on the preferences above:</p><br>
-                    <button type="submit" value="submit" class="submit_btn">GO</button>
+                    <input type="button" value="GO" class="submit_btn">
                 </form>
 
             </section>
