ca4a71e9340c9561a26ac377001858ab4dc37496
Nikkala Thomson <tho15058@byui.edu>
2019-06-03 04:05:32 -0400

    testing

--- web/boardgames/action_page.php
+++ web/boardgames/action_page.php
@@ -95,8 +95,8 @@ else if (isset($_POST['p_display_name'])){
         }
         
         // Redirect to games page
- header("Location: games.php");
- exit();
+ //header("Location: games.php");
+ //exit();
     }
                 
 
