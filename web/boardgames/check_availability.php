d946fb453a0f6d81acf2ed6847d9d1ec586a373f
Nikkala Thomson <tho15058@byui.edu>
2019-06-06 10:10:07 -0400

    testing

--- web/boardgames/check_availability.php
+++ web/boardgames/check_availability.php
@@ -2,6 +2,10 @@
 // Get the Heroku database
 require_once "db_connect.php";
 $db = get_db();
+// Force display of all errors (for debugging)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 if(!empty($_POST["username"])) {
     $username = htmlspecialchars($_POST['username']);
@@ -9,8 +13,8 @@ if(!empty($_POST["username"])) {
     $statement->bindValue(':username', $username, PDO::PARAM_STR);
     $statement->execute();
     $duplicate_gamer = $statement->fetch(PDO::FETCH_ASSOC);
-  if (1==1){
-      echo "<span class='status-not-available'> Username Not Available. testing</span>" ;
+  if ($duplicate_gamer){
+      echo "<span class='status-not-available'> Username Not Available. </span>" ;
   } else {
       echo "<span class='status-available'> Username Available.</span>";
   }
