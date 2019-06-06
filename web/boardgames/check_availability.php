95d01f41e32027cbaa8e27a0354c344f51b5b73d
Nikkala Thomson <tho15058@byui.edu>
2019-06-06 10:14:29 -0400

    testing

--- web/boardgames/check_availability.php
+++ web/boardgames/check_availability.php
@@ -1,15 +1,15 @@
 <?php
 // Get the Heroku database
-require_once "db_connect.php";
+require_once 'db_connect.php';
 $db = get_db();
 // Force display of all errors (for debugging)
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if(!empty($_POST["username"])) {
+if(!empty($_POST['username'])) {
     $username = htmlspecialchars($_POST['username']);
-    $statement = $db->prepare('SELECT * FROM gamer WHERE username=:username;');
+    $statement = $db->prepare('SELECT * FROM gamer WHERE username = :username;');
     $statement->bindValue(':username', $username, PDO::PARAM_STR);
     $statement->execute();
     $duplicate_gamer = $statement->fetch(PDO::FETCH_ASSOC);
