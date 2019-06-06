0c2fab1bc4efc8ef558a2e6e086a32f9cbbd925a
Nikkala Thomson <tho15058@byui.edu>
2019-06-06 10:00:39 -0400

    testing

--- web/boardgames/check_availability.php
+++ web/boardgames/check_availability.php
@@ -4,10 +4,11 @@ require_once "db_connect.php";
 $db = get_db();
 
 if(!empty($_POST["username"])) {
-  $statement1 = $db->prepare('SELECT * FROM gamer WHERE username=:username;');
-    $statement1->bindValue(':username', $username, PDO::PARAM_STR);
-    $statement1->execute();
-    $duplicate_gamer = $statement1->fetch(PDO::FETCH_ASSOC);
+    $username = htmlspecialchars($_POST['username']);
+    $statement = $db->prepare('SELECT * FROM gamer WHERE username=:username;');
+    $statement->bindValue(':username', $username, PDO::PARAM_STR);
+    $statement->execute();
+    $duplicate_gamer = $statement->fetch(PDO::FETCH_ASSOC);
   if (!empty($duplicate_gamer)){
       echo "<span class='status-not-available'> Username Not Available.</span>";
   }else{
