9b2438850890afc40c0eb964bfdac9f2ce1c100b
Nikkala Thomson <tho15058@byui.edu>
2019-06-06 20:33:23 -0400

    testing

--- web/boardgames/action_page.php
+++ web/boardgames/action_page.php
@@ -88,16 +88,14 @@ else if (isset($_POST['p_display_name'])){
                 $statement->bindValue(':email', $email, PDO::PARAM_STR);
             }
             $statement->execute(); 
-              echo "right password";
-            // Redirect to games page
-           // header("Location: games.php");
-            //exit();   
+           // Redirect to games page
+            header("Location: games.php");
+            exit();   
            
             } else {
               // Redirect to update page 
-          //  header("Location: edit_profile.php");
-          //  exit();  
-                    echo "wrong password";
+            header("Location: edit_profile.php");
+            exit();  
             }
     }   
                     
