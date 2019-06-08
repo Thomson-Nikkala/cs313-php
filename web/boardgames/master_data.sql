122e751002a5ff1d53e9a871fb22b2fd40196a84
Nikkala Thomson <tho15058@byui.edu>
2019-06-08 11:48:24 -0400

    testing

---
+++ web/boardgames/master_data.sql
@@ -0,0 +1,15 @@
+-- Guest input statement for the user table.  
+
+INSERT INTO gamer (username, display_name, email, hashed_password)
+VALUES ('Guest', 'Guest', 'guest@gmail.com', 'gobbledygook');
+
+-- Guest input for the preference table
+
+INSERT INTO preference(gamer, preferences)
+VALUES (1, '{ "min_players":2, "max_players":4, "min_playtime":30, "max_playtime":120, "min_weight":1.5, "max_weight":2.5, "themes":[], "mechanisms":[]}');
+
+
+-- input for the board_game table
+
+INSERT INTO board_game (name, designer, description, image_url, properties)
+VALUES ('Azul', 'Michael Kiesling', 'Draft colored tiles and decorate the walls of your palace.  Score points for completing patterns and sets.', 'https://cf.geekdo-images.com/itemrep/img/ql-0-t271LVGqbmWA1gdkIH7WvM=/fit-in/246x300/pic3718275.jpg', '{ "min_players":2, "max_players":4, "min_playtime":30, "max_playtime":45,  "weight":1.8, "themes":["Art", "Renaissance"], "mechanisms":[ "Drafting", "Pattern_Building", "Set_Collection", "Tile_Placement"]}' );
---
+++ web/boardgames/recommendation.php
@@ -0,0 +1,70 @@
+<?php
+// Start the session
+session_start();
+// Initialize session variables
+if (!isset($_SESSION['gamer'])) {
+    $_SESSION['gamer'] = 1;
+}
+if (!isset($_SESSION['best_game'])) {
+    $_SESSION['best_game'] = 0;
+}
+if (!isset($_SESSION['best_game_score'])) {
+    $_SESSION['best_game_score'] = 0;
+}
+// Get the Heroku database
+require_once "db_connect.php";
+$db = get_db();
+
+?>
+
+<!DOCTYPE html>
+<!--
+This is the login page for the Board Game Whisperer
+Author: Nikkala Thomson
+-->
+
+<html lang="en-us">
+
+<head>
+    <?php $ROOT = '../';
+    include '../modules/head.php'; ?>
+    <title>The Board Game Whisperer</title>
+</head>
+
+<body>
+    <header>
+        <div id="header-band"></div>
+        <div id="header-text" class="center-block">
+            <h1>Your Game Recommendation</h1>
+        </div>
+    </header>
+    <div class=center-block>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="edit_profile.php">Profile</a></li>
+                <li><a href="games.php">Get Games</a></li>
+            </ul>
+        </nav>
+        <main>
+
+
+            <section class="wide-section">
+                <br>
+                <?php echo $_SESSION['best_game'] ?>
+
+                <form action="games.php" method="post">
+                    <button type="submit" name="submit" class="submit_btn">Get Another Game Recommendation</button>
+                </form>
+                <p>Note: If you are logged in as "Guest," you will need to register before getting multiple recommendations.</p>
+            </section>
+        </main>
+        <footer>
+            <?php include("../modules/footer.php"); ?>
+        </footer>
+    </div>
+</body>
+
+</html>
