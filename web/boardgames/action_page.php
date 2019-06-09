435d0baeefe05fa1bb94f2e23d425821e28fa121
Nikkala Thomson <tho15058@byui.edu>
2019-06-08 21:19:56 -0400

    testing

--- web/boardgames/action_page.php
+++ web/boardgames/action_page.php
@@ -254,19 +254,20 @@ if (isset($_POST['go'])) {
         
         if ($game_score >= $best_game_score) {
             // check if this game has already been recommended to this gamer
-            $already_recommended = FALSE;
+            $already_recommended = 'false';
             $statement3 = $db->prepare("SELECT * FROM recommendation WHERE gamer = $gamer");
             $statement3->execute();
             $recommendations = $statement3->fetchAll(PDO::FETCH_ASSOC);
+            echo $board_game['board_game'];
             foreach ($recommendations AS $recommendation) {
                 if ($recommendation['board_game']==$board_game['board_game']) {
-                    $already_recommended = TRUE;
+                    $already_recommended = 'true';
                     echo 'in true';
                 }
             }
-         
+         echo $already_recommended;
             // if not, set this game to the best game 
-            if ($already_recommended==FALSE){
+            if ($already_recommended=='false'){
                 $best_game_score = $game_score;
                 $best_board_game = $board_game['board_game'];
             }
