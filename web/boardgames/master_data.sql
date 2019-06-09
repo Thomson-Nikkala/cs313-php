1bd9bc9e4d498f23513f6dbbb199f72003a3691e
Nikkala Thomson <tho15058@byui.edu>
2019-06-08 22:40:03 -0400

    testing

--- web/boardgames/master_data.sql
+++ web/boardgames/master_data.sql
@@ -12,7 +12,7 @@ VALUES (1, '{"min_players":2, "max_players":4, "min_playtime":30, "max_playtime"
 -- input for the board_game table
 
 INSERT INTO board_game (name, image_url, properties)
-VALUES ('Azul', 'https://images-na.ssl-images-amazon.com/images/I/61Uzk6kyVyL.jpg', '{ "min_players":2, "max_players":4, "min_playtime":30, "max_playtime":45,  "weight":1.8, "themes":["Art", "Renaissance"], "mechanisms":[ "Drafting", "Pattern_Building", "Set_Collection", "Tile_Placement"]}' );
+VALUES ('Azul', 'https://cf.geekdo-images.com/opengraph/img/5UBOdYQkBGcTlZFSWH3cQ2G3fvw=/fit-in/1200x630/pic3718275.jpg', '{ "min_players":2, "max_players":4, "min_playtime":30, "max_playtime":45,  "weight":1.8, "themes":["Art", "Renaissance"], "mechanisms":[ "Drafting", "Pattern_Building", "Set_Collection", "Tile_Placement"]}' );
 
 VALUES ('', '', '{ "min_players":, "max_players":, "min_playtime":, "max_playtime":,  "weight":, "themes":["", ""], "mechanisms":[ "", ""]}' );
 
